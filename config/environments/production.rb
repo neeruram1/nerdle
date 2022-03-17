cd8927d0fcb4e58426fa7db7104aaca11fa3d7ca
Nirupama Ram <nram@doximity.com>
2022-03-17 11:28:55 -0600

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "wordle_production"
+  # config.active_job.queue_name_prefix = "nerdle_production"
 
   config.action_mailer.perform_caching = false
 
